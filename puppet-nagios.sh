a720350370af21dac8444d5c20d041ce2f5b578d
marcindulak <marcin.dulak@gmail.com>
2015-12-07 13:18:19 +0100

    make sure it's a failure

--- puppet-nagios.sh
+++ puppet-nagios.sh
@@ -4,7 +4,7 @@ git clone https://github.com/marcindulak/puppet-nagios.git && \
 cd puppet-nagios && \
 vagrant up && \
 vagrant ssh nagiosserver -c "sudo su -c 'service httpd start'" && \
-vagrant ssh nagiosserver -c "sudo su -c '/usr/lib64/nagios/plugins/check_http -H centos6'" && \
+! vagrant ssh nagiosserver -c "sudo su -c '/usr/lib64/nagios/plugins/check_http -H centos6'" && \
 vagrant ssh centos6 -c "sudo su -c 'yum install -y httpd'" && \
 vagrant ssh centos6 -c "sudo su -c 'touch /var/www/html/index.html'" && \
 vagrant ssh centos6 -c "sudo su -c 'chown apache.apache /var/www/html/index.html'" && \
