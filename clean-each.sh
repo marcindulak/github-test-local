0af956171ecd64ebd7d048eda3a67db193f5e353
marcindulak <marcin.dulak@gmail.com>
2016-01-23 13:09:52 +0100

    delete pool not volumes

--- clean-each.sh
+++ clean-each.sh
@@ -2,6 +2,4 @@ killall VBoxHeadless 2> /dev/null
 for interface in `VBoxManage list hostonlyifs | egrep '^Name:' | cut -d: -f2`; do VBoxManage hostonlyif remove $interface; done
 for net in `virsh -q net-list | awk '{print $1}'`; do virsh net-destroy $net; done
 virsh net-autostart default --disable
-for vol in `virsh -q vol-list default | awk '{print $1}'`; do virsh vol-delete $vol default; done
-virsh pool-destroy default
-virsh pool-undefine default
+for pool in `virsh -q pool-list | awk '{print $1}'`; do virsh pool-destroy $pool; virsh pool-undefine $pool; done
