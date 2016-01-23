dd0d1d5228f0ff84794c5f464debf9fe956079da
marcindulak <marcin.dulak@gmail.com>
2016-01-23 11:07:45 +0100

    delete libvirt storage volumes

--- clean-each.sh
+++ clean-each.sh
@@ -1,3 +1,4 @@
 killall VBoxHeadless 2> /dev/null
 for interface in `VBoxManage list hostonlyifs | egrep '^Name:' | cut -d: -f2`; do VBoxManage hostonlyif remove $interface; done
 for net in `virsh -q net-list | awk '{print $1}'`; do virsh net-destroy $net; done
+for vol in `virsh -q vol-list default | awk '{print $1}'`; do virsh vol-delete $vol default; done
