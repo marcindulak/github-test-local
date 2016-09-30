c062f623bed62fe9be5ede2acdc38b110b2da5cb
Marcin Dulak <marcin.dulak@gmail.com>
2016-09-30 16:17:32 +0200

    curl http not https

--- vagrant-CaltechDelftX-QuCryptox.sh
+++ vagrant-CaltechDelftX-QuCryptox.sh
@@ -2,5 +2,5 @@ git clone https://github.com/marcindulak/vagrant-CaltechDelftX-QuCryptox.git && 
 cd vagrant-CaltechDelftX-QuCryptox && \
 vagrant up && \
 vagrant ssh -c "systemctl status IJulia" && \
-curl -v https://localhost:8890/ && \
+curl -m 5 -v http://localhost:8890/ && \
 vagrant destroy -f
