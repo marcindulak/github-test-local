f1d6297018c1212ab452dd22a04338d8404f2dd4
Marcin Dulak <marcin.dulak@gmail.com>
2017-02-18 20:12:48 +0100

    update tests

--- vagrant-munin-tutorial.sh
+++ vagrant-munin-tutorial.sh
@@ -3,12 +3,13 @@ cd vagrant-munin-tutorial && \
 vagrant up && \
 sleep 360 && \
 curl --user munin:munin http://localhost:8080/munin/ && \
-vagrant ssh centos6 -c "sudo su -c 'cd /etc/munin/plugins/; ln -s /usr/share/munin/plugins/apache_accesses'" && \
-vagrant ssh centos6 -c "sudo su -c 'cd /etc/munin/plugins/; ln -s /usr/share/munin/plugins/apache_processes'" && \
-vagrant ssh centos6 -c "sudo su -c 'cd /etc/munin/plugins/; ln -s /usr/share/munin/plugins/apache_volume'" && \
-vagrant ssh centos6 -c "sudo su -c 'echo \"[apache_*]\" > /etc/munin/plugin-conf.d/apache'" && \
-vagrant ssh centos6 -c "sudo su -c 'service munin-node reload'" && \
-vagrant ssh centos6 -c "sudo su -c 'service httpd start; service httpd restart'" && \
+vagrant ssh centos7 -c "sudo su -c 'cd /etc/munin/plugins/; ln -s /usr/share/munin/plugins/apache_accesses'" && \
+vagrant ssh centos7 -c "sudo su -c 'cd /etc/munin/plugins/; ln -s /usr/share/munin/plugins/apache_processes'" && \
+vagrant ssh centos7 -c "sudo su -c 'cd /etc/munin/plugins/; ln -s /usr/share/munin/plugins/apache_volume'" && \
+vagrant ssh centos7 -c "sudo su -c 'echo \"[apache_*]\" > /etc/munin/plugin-conf.d/apache'" && \
+vagrant ssh centos7 -c "sudo su -c 'service munin-node restart'" && \
+! vagrant ssh centos7 -c "sudo su -c 'curl http://localhost/'" && \
+vagrant ssh centos7 -c "sudo su -c 'service httpd restart'" && \
 sleep 360 && \
-vagrant ssh centos6 -c "sudo su -c 'curl http://localhost/'" && \
+vagrant ssh centos7 -c "sudo su -c 'curl http://localhost/'" && \
 vagrant destroy -f
