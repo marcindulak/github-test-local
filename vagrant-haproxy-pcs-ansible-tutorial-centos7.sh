b11eec6d6d647b012d7d6aa905ce696206d89fce
Marcin Dulak <marcin.dulak@gmail.com>
2017-06-27 19:00:08 +0200

    allow ansible-playbook to finish with errors and continue

--- vagrant-haproxy-pcs-ansible-tutorial-centos7.sh
+++ vagrant-haproxy-pcs-ansible-tutorial-centos7.sh
@@ -7,7 +7,7 @@ vagrant plugin install landrush && \
 vagrant up || : && \
 sleep 30 && \
 vagrant ssh mgt1.mydomain -c "curl -sgG --data-urlencode query='up{instance=~\"app.*\", job=\"node_exporter\"}' localhost:9090/api/v1/query | python -m json.tool" && \
-vagrant ssh mgt1.mydomain -c "ansible-playbook -i /vagrant/ansible/hosts.yml /vagrant/ansible/playbook.yml" && \
+vagrant ssh mgt1.mydomain -c "ansible-playbook -i /vagrant/ansible/hosts.yml /vagrant/ansible/playbook.yml" || : && \
 vagrant provision mgt1.mydomain && \
 vagrant ssh mgt1.mydomain -c "curl loadbalancer:80" && \
 curl localhost:40080 && \
@@ -22,7 +22,7 @@ sleep 30 && \
 vagrant ssh mgt1.mydomain -c "curl app1:8080" | grep "Hello World" && \
 vagrant destroy -f app1.mydomain && \
 vagrant up app1.mydomain && \
-vagrant ssh mgt1.mydomain -c "ansible-playbook -i /vagrant/ansible/hosts.yml /vagrant/ansible/playbook.yml.rollback" && \
+vagrant ssh mgt1.mydomain -c "ansible-playbook -i /vagrant/ansible/hosts.yml /vagrant/ansible/playbook.yml.rollback" || : && \
 sleep 30 && \
 vagrant ssh mgt1.mydomain -c "curl app1:8080" | grep "Greetings from Spring Boot!" && \
 ACTIVE_LB=`vagrant ssh lb1.mydomain -c "sudo su - -c \"pcs status | grep virtual-ip | cut -d: -f5 | cut -d' ' -f2 | tr -d '\n'\""` && \
@@ -34,6 +34,6 @@ vagrant up $ACTIVE_LB && \
 vagrant ssh $PASSIVE_LB -c "scp -o StrictHostKeyChecking=no -p /etc/corosync/corosync.conf $ACTIVE_LB:/tmp" && \
 vagrant ssh $ACTIVE_LB -c "sudo su - -c 'mkdir /etc/corosync&& mv /tmp/corosync.conf /etc/corosync&& chown root.root /etc/corosync/corosync.conf'" && \
 vagrant ssh $ACTIVE_LB -c "sudo su - -c 'semanage fcontext -a -s system_u -t etc_t /etc/corosync/corosync.conf'" && \
-vagrant ssh mgt1.mydomain -c "ansible-playbook -i /vagrant/ansible/hosts.yml /vagrant/ansible/playbook.yml.rollback" && \
+vagrant ssh mgt1.mydomain -c "ansible-playbook -i /vagrant/ansible/hosts.yml /vagrant/ansible/playbook.yml.rollback" || : && \
 vagrant ssh $ACTIVE_LB -c "sudo su - -c 'pcs status'" && \
 vagrant destroy -f
