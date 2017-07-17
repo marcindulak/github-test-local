c7b0787e163a96ed9f579cac5bda7d0b2f90f472
Marcin Dulak <marcin.dulak@gmail.com>
2017-07-17 16:22:14 +0200

    collect debug information after the VM is launched

--- vagrant-opennebula-tutorial-centos7.sh
+++ vagrant-opennebula-tutorial-centos7.sh
@@ -8,7 +8,7 @@ vagrant ssh frontend -c "sudo su - oneadmin -c 'echo NAME = private > mynetwork.
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet list'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet create mynetwork.one'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet list'" && \
-vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage create --name ttylinux --path http://marketplace.c12g.com/appliance/4fc76a938fb81d3517000003/download --datastore default --prefix hd'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage create --name ttylinux --path http://marketplace.opennebula.systems/appliance/4fc76a938fb81d3517000003/download --datastore default --prefix hd'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage list'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate create --name ttylinux --cpu 1 --vcpu 1 --memory 127 --arch x86_64 --disk ttylinux --nic private --vnc --ssh --net_context'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate list'" && \
@@ -16,6 +16,9 @@ sleep 30 && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate instantiate ttylinux'" && \
 sleep 300 && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevm list'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage show 0'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate show 0'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onevm show 0'" && \
 vagrant ssh frontend -c "sudo su - -c 'yum -y install sshpass'" && \
 vagrant ssh frontend -c "sshpass -p password ssh -o StrictHostKeyChecking=no root@192.168.10.100 '/sbin/ifconfig eth0'" && \
 vagrant destroy -f
