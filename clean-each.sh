ea1b1ff59039c8b7a0ac25da73f431ebf79f570f
Marcin Dulak <marcin.dulak@gmail.com>
2017-07-18 12:52:28 +0200

    update opennebula test; kill libvirt stale forwarded ports

--- clean-each.sh
+++ clean-each.sh
@@ -2,5 +2,7 @@ killall VBoxHeadless 2> /dev/null
 for interface in `VBoxManage list hostonlyifs | egrep '^Name:' | cut -d: -f2`; do VBoxManage hostonlyif remove $interface; done
 for net in `virsh -q net-list --all | awk '{print $1}'`; do virsh net-destroy $net; done
 virsh net-autostart default --disable
+# kill libvirt stale forwarded ports
+for p in `ps aux | grep libv | grep -v grep | awk '{print $1, $2}' | grep $USER | awk '{print $2}'`; do echo kill $p&& kill $p; done
 for pool in `virsh -q pool-list | awk '{print $1}'`; do virsh pool-destroy $pool; virsh pool-undefine $pool; done
 for guest in `virsh -q list --inactive | awk '{print $2}'`; do virsh undefine $guest; done
--- vagrant-opennebula-tutorial-centos7.sh
+++ vagrant-opennebula-tutorial-centos7.sh
@@ -4,21 +4,26 @@ vagrant plugin install vagrant-libvirt && \
 vagrant up --no-parallel && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onehost create node1 -i kvm -v kvm'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onehost list'" && \
-vagrant ssh frontend -c "sudo su - oneadmin -c 'echo NAME = private > mynetwork.one; echo VN_MAD = dummy >> mynetwork.one; echo BRIDGE = br1 >> mynetwork.one; echo AR = [TYPE = IP4, IP = 192.168.10.100, SIZE = 3] >> mynetwork.one'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'echo NAME = private > private.one; echo VN_MAD = dummy >> private.one; echo BRIDGE = br1 >> private.one; echo AR = [TYPE = IP4, IP = 192.168.10.100, SIZE = 3] >> private.one'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet list'" && \
-vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet create mynetwork.one'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet create private.one'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet list'" && \
-vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage create --name ttylinux --path http://marketplace.opennebula.systems/appliance/4fc76a938fb81d3517000003/download --datastore default --prefix hd'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage create --name testvm --path http://marketplace.opennebula.systems/appliance/4e3b2788-d174-4151-b026-94bb0b987cbb/download --datastore default --prefix vd --driver qcow2'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage list'" && \
-vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate create --name ttylinux --cpu 1 --vcpu 1 --memory 127 --arch x86_64 --disk ttylinux --nic private --vnc --ssh --net_context'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate create --name testvm --cpu 1 --vcpu 1 --memory 256 --arch x86_64 --disk testvm --nic private --vnc --ssh --net_context'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate list'" && \
 sleep 30 && \
-vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate instantiate ttylinux'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'echo CONTEXT = [ USERNAME = root, PASSWORD = password, NETWORK = YES ] > context.one'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate update testvm -a context.one'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate instantiate testvm'" && \
 sleep 300 && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevm list'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onehost show 0'" && \
+vagrant ssh frontend -c "sudo su - oneadmin -c 'onevnet show 0'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'oneimage show 0'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onetemplate show 0'" && \
 vagrant ssh frontend -c "sudo su - oneadmin -c 'onevm show 0'" && \
+vagrant ssh node1 -c "sudo su - -c 'virsh dumpxml one-0'" && \
 vagrant ssh frontend -c "sudo su - -c 'yum -y install sshpass'" && \
-vagrant ssh frontend -c "sshpass -p password ssh -o StrictHostKeyChecking=no root@192.168.10.100 '/sbin/ifconfig eth0'" && \
+vagrant ssh frontend -c "sshpass -p password ssh -o StrictHostKeyChecking=no root@192.168.10.100 '/sbin/ifconfig'" && \
 vagrant destroy -f
